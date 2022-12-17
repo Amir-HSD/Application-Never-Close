6a86588867b01b4056267ea7f2715ca83f7d368c
Ali Ahmadi <mrkiller1383@gmail.com>
2022-12-17 14:39:31 +0330

    fullscreen support

--- app/src/main/java/com/example/myapplication589/MainActivity.java
+++ app/src/main/java/com/example/myapplication589/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication589;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
+import android.os.Build;
 import android.os.Bundle;
 import android.content.Intent;
 import android.text.Editable;
@@ -23,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if(Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
 
         Intent service = new Intent(getApplicationContext(), MyService.class);
         startService(service);
